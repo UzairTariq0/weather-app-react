8e929aeda445f7940e5c2519e003878ddbfbd41c
Uzair <uzair.t2006@gmail.com>
2022-11-05 20:25:57 +0500

    Update the Temp.js

--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import "./temp.css"
+import axios from 'axios'
 
 const Temp = () => {
-  const [city, setCity] = useState("Lahore")
-  // const [search, setSearch] = useState("Karachi")
+  const [data, setData] = useState([])
+  const [location, setLocation] = useState('')
 
 
+  let url =  `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ac278467ff1288d6a0d5b9fd5ad3facf`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        // console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
   return (
     <div className='weather-app'>
       <div className="card">
         <div className="search">
-          <input type="text" className='search-bar' placeholder='Search' />
-          <button>
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text"
+          className='search-bar'
+          />
         </div>
         <div className="weather">
-          <h2 className='city'>{city}</h2>
-          <div className="temp">21.4&#176;C</div>
+          <h2 className='city'>{data.name}</h2>
+          {data.main ? <p className='temp'>{data.main.temp.toFixed()}°C</p> : null}
           <div className="bottom">
-            <div className="description">Cloudy</div>
-            <div className="humidity">Humidity: 18%</div>
-            <div className="wind">Wind speed: 7.2 km/h</div>
+          {data.weather ? <p className='description'>{data.weather[0].main}</p> : null}
+          {data.main ? <p className='humidity'>Humidity: {data.main.humidity}%</p> : null}
+          {data.wind ? <p className='wind'>Wind Speed: {data.wind.speed.toFixed()} MPH</p> : null}
           </div>
         </div>
       </div>
@@ -29,4 +45,4 @@ const Temp = () => {
   )
 }
 
-export default Temp
+export default Temp;
